83bc90188f087f12855ea06695ae59b8930a9873
Aditya Verma <aaadarths2357@gmail.com>
2020-10-07 17:17:03 +0530

    Refactor: inserting element after a given element in the linked list

--- src/test/java/com/bridgelabz/linkedlistimplementation/MyLinkedListTest.java
+++ src/test/java/com/bridgelabz/linkedlistimplementation/MyLinkedListTest.java
@@ -121,4 +121,22 @@ public class MyLinkedListTest {
 		myLinkedList.printMyList();
 		Assert.assertEquals(secondNode, searchedNode);
 	}
+
+	@Test
+	public void given3NumberWhenInsertingAfterAnElementShouldPassLinkedListTest() {
+		MyNode<Integer> firstNode = new MyNode(56);
+		MyNode<Integer> secondNode = new MyNode(30);
+		MyNode<Integer> thirdNode = new MyNode(70);
+
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.append(firstNode);
+		myLinkedList.append(secondNode);
+		myLinkedList.append(thirdNode);
+		myLinkedList.insertAfter(30, 40);
+		boolean result = myLinkedList.getHead().equals(firstNode) && myLinkedList.getHead().getNext().equals(secondNode)
+				&& myLinkedList.getHead().getNext().getNext().equals(myLinkedList.search(40))
+				&& myLinkedList.getTail().equals(thirdNode);
+		myLinkedList.printMyList();
+		Assert.assertTrue(result);
+	}
 }
