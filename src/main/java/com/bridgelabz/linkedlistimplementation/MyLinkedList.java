49fa39eb97c745fdac5e5ad020c014bf4196638c
Aditya Verma <aaadarths2357@gmail.com>
2020-10-07 13:26:10 +0530

    Added: welcome to linked list implementation program to check project configuration

---
+++ src/main/java/com/bridgelabz/linkedlistimplementation/MyLinkedList.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.linkedlistimplementation;
+
+public class MyLinkedList {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Linked List Implementation Program");
+	}
+}
